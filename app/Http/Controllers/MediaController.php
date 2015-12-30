3d5f67e234b14141a899f354deeeb4d94675668b
erikkacsm <ebaker@mymail.mines.edu>
2015-12-29 17:32:25 -0700

    use dropdowns in new media form

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -11,8 +11,19 @@ class MediaController extends Controller
 {
 	public function createForm() {
 		$seriesAbbrToName = \App\Series::get()->pluck('seriesName', 'seriesAbbreviation')->toArray();
+		$series = \App\Series::get()->pluck('seriesAbbreviation');
+		$seriesToCollections = array();
+		foreach($series as $thisSeries) {
+			$seriesToCollections[$thisSeries] =
+							\App\Media::select('collection')
+							->where('series', $thisSeries)
+							->distinct()
+							->get()
+							->pluck('collection')
+							->toArray();
+		}
 		
-		return view('forms/newMedia')->with(['seriesAbbrToName'=>$seriesAbbrToName]);
+		return view('forms/newMedia')->with(['seriesAbbrToName'=>$seriesAbbrToName, 'series'=>$series, 'seriesToCollections'=>$seriesToCollections]);
 	}
 	
     public function create() {
--- resources/views/forms/newMedia.blade.php
+++ resources/views/forms/newMedia.blade.php
@@ -5,22 +5,49 @@
 	<!-- New event form -->
 	<form role="form" method="POST" action="{{ url('/newMedia') }}">
 		{!! csrf_field() !!}
+		
+		<!-- Name -->
 		<div class='label'>Name</div>
 		<input name="name" class='input' type="text">
+		
+		<!-- Credit -->
 		<div class='label'>Credit (Author/Director/Developer)</div>
 		<input name="credit" class='input' type="text">
+		
+		<!-- Series -->
 		<div class='label'>Series</div>
-		<input name="series" class='input' type="text">
+		<select class='input'>
+			@foreach($series as $aSeries)
+				<option name='series' value='{{ $aSeries }}'>{{ $seriesAbbrToName[$aSeries] }}</option>
+			@endforeach
+			<option value='newSeries'>New Series</option>
+		</select>
+		
+		<!-- Collection -->
 		<div class='label'>Collection</div>
-		<input name="collection" class='input' type="text">
+		<select class='input'>
+			<option>list of collections</option>
+		</select>
+		
+		<!-- Number in Collection -->
 		<div class='label'>Number in Collection</div>
-		<input name="numberInCollection" class='input' type="text">
+		<input name="numberInCollection" class='input' type="number">
+		
+		<!-- Medium -->
 		<div class='label'>Medium</div>
-		<input name="medium" class='input' type="text">
+		<select class='input'>
+			<option>list of mediums</option>
+		</select>
+		
+		<!-- Summary -->
 		<div class='label'>Summary</div>
 		<textarea name="summary" class='input text-area'></textarea>
+		
+		<!-- Timeline Date -->
 		<div class='label'>Date in Timeline</div>
 		<input name="timelineDate" class='input' type='date'>
+		
+		<!-- Save/Reset/Cancel Buttons -->
 		<div class='buttonHolder'>
 			<button type='submit' class='formButton'>Save</button>
 			<button type='reset' class='formButton'>Reset</button>
