888f5120739b290d89ea2630d3f36c084bc4cbd2
erikkacsm <ebaker@mymail.mines.edu>
2016-01-02 14:34:34 -0700

    styled show media view

--- resources/views/show/media.blade.php
+++ resources/views/show/media.blade.php
@@ -1,24 +1,51 @@
 @extends('show/master')
 
 @section('showContent')
-	<p class='name'>{{ $media['name'] }}</p>
+	<div class='panelHolder'>
+		<p class='name'>{{ $media['name'] }}</p>
+		
+		<p class='collection'>
+		{{ $media['series'] }} - 
+		{{ $media['collection'] }}
+		@if($media['numberInCollection'] != NULL)
+			<p class='collection number'>{{ $media['numberInCollection'] }}</p>
+		@endif
+		</p>
+	</div>
+	<hr class='clear'>
 	
-	<p class='collection'>
-	{{ $media['series'] }} - 
-	{{ $media['collection'] }}
-	@if($media['numberInCollection'] != NULL)
-		<p class='collection number'>{{ $media['numberInCollection'] }}</p>
-	@endif
-	</p>
+	<div class='panelHolder'>
+		<div class='leftPanel'>
+			<p class='label'>Summary</p>
+			<p class='attribute'>{{ $media['summary'] }}</p>
+		</div>
+		
+		<div class='rightPanel'>
+			<p class='label'>Relevant Events</p>
+			<ul>
+				@foreach ($events as $event)
+					<li>{{ $event['name'] }}</li>
+				@endforeach
+			</ul>
+		</div>
+	</div>
+	<hr class='clear'>
 	
-	<p class='label'>Credit</p>
-	<p>{{ $media['credit'] }}</p>
-	{{ $media['medium'] }}<br>
-	{{ $media['summary'] }}<br>null
-	{{ $media['timelineDate'] }}<br>
+	<div class='panelHolder'>
+		<div class='bottomLeft'>
+			<p class='label'>Medium</p>
+			<p class='attribute'>{{ $media['medium'] }}</p>
+		</div>
+		
+		<div class='bottomRight'>
+			<p class='label'>Credit</p>
+			<p class='attribute'>{{ $media['credit'] }}</p>
+		</div>
+		
+		<div class='bottomCenter'>
+			<p class='label'>Date in Timeline</p>
+			<p class='attribute'>{{ $media['timelineDate'] }}</p>
+		</div>
+	</div>
 	
-	Relevant Events:<br>
-	@foreach ($events as $event)
-		{{ $event['name'] }}<br>
-	@endforeach
 @endsection
